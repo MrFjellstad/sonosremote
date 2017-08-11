172959de1cd323b23010d55cc0b4110995181622
Kent Fjellstad <kent.fjellstad@amedia.no>
2017-08-11 08:39:22 +0200

    Added logging framework

--- bin/index.js
+++ bin/index.js
@@ -1,41 +1,42 @@
 const cron = require('node-schedule');
 const Dropbox = require('dropbox');
 const sonos = require('sonos');
+const bunyan = require('bunyan');
+const log = bunyan.createLogger({name: 'SonosRemote'});
 
 const config = require('./config');
 
 const dbx = new Dropbox({ accessToken: config.get('accesstoken') });
 
-// cron.scheduleJob('/3 * * * * *', () => {
 const schedule = cron.scheduleJob(config.get('schedule'), () => {
     dbx.filesListFolder({ path: config.get('dropboxPath') })
         .then((response) => {
             if (response.entries.length > 0) {
                 const search = sonos.search((device) => {
                     search.destroy((searchResponse) => {
-                        console.log('Stopped searching');
-                        console.log(searchResponse);
+                        log.info('Stopped searching');
+                        log.info(searchResponse);
                     });
 
                     device.play((playResponse) => {
-                        console.log(playResponse);
+                        log.info(playResponse);
                         return true;
                     });
                 });
 
                 response.entries.forEach((element) => {
-                    console.log(element.name);
+                    log.info(element.name);
                     const fileFullPath = {
                         path: `${config.get('dropboxPath')}/${element.name}`,
                     };
                     dbx.filesDelete(fileFullPath);
                 });
             } else {
-                console.log('Nothing here');
+                log.info('Nothing here');
             }
         })
         .catch((error) => {
-            console.log(error.error);
+            log.warn(error.error);
             schedule.cancel();
         });
 });

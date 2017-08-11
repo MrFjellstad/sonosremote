e8c971c17cf0e249e69578bac282c05eafe93fc7
Kent Fjellstad <kent.fjellstad@amedia.no>
2017-08-11 08:34:08 +0200

    Added eslint

--- bin/config.js
+++ bin/config.js
@@ -1,39 +1,39 @@
-var convict = require('convict');
+const convict = require('convict');
 
 // Define a schema
-var config = convict({
+const config = convict({
     env: {
         doc: 'The applicaton environment.',
         format: [
-            'production', 'development', 'test'
+            'production', 'development', 'test',
         ],
         default: 'development',
-        env: 'NODE_ENV'
+        env: 'NODE_ENV',
     },
     accesstoken: {
         doc: 'Dropbox accestoken.',
         format: 'String',
         default: 'SECRET',
-        env: 'ACCESSTOKEN'
+        env: 'ACCESSTOKEN',
     },
     dropboxPath: {
         doc: 'The folder the app will access to look for the file from IFTTT.',
         format: 'String',
         default: '/IFTTT/SonosControl',
-        env: 'DROPBOX_PATH'
-        },
+        env: 'DROPBOX_PATH',
+    },
     schedule: {
         doc: 'Crontab timeformat',
         format: 'String',
-        default: '/3 * * * * *'
-    }
+        default: '/3 * * * * *',
+    },
 });
 
 // Load environment dependent configuration
-var env = config.get('env');
-config.loadFile('./config/' + env + '.json');
+const env = config.get('env');
+config.loadFile(`./config/${env}.json`);
 
 // Perform validation
-config.validate({allowed: 'strict'});
+config.validate({ allowed: 'strict' });
 
 module.exports = config;
--- bin/index.js
+++ bin/index.js
@@ -4,43 +4,38 @@ const sonos = require('sonos');
 
 const config = require('./config');
 
-// const dbx = new Dropbox({accessToken: 'OYC94uhlHVAAAAAAAAAE5BWE_S5B7_zjQyDdwSMjAK8WcFOsZkILl9I8HQg8j9Fa'});
-console.log(config.get('accesstoken'));
-const dbx = new Dropbox({accessToken: config.get('accesstoken')});
+const dbx = new Dropbox({ accessToken: config.get('accesstoken') });
 
 // cron.scheduleJob('/3 * * * * *', () => {
 const schedule = cron.scheduleJob(config.get('schedule'), () => {
-    console.log('Checking for stuff');
-    // dbx.filesListFolder({path: '/IFTTT/SonosControl'}).then((response) => {
-    dbx.filesListFolder({path: config.get('dropboxPath')})
-    .then((response) => {
-        if (response.entries.length > 0) {
-            console.log('Found files');
-            const search = sonos.search((device) => {
-                search.destroy((response) => {
-                    console.log('Stopped searching');
-                    console.log(response);
+    dbx.filesListFolder({ path: config.get('dropboxPath') })
+        .then((response) => {
+            if (response.entries.length > 0) {
+                const search = sonos.search((device) => {
+                    search.destroy((searchResponse) => {
+                        console.log('Stopped searching');
+                        console.log(searchResponse);
+                    });
+
+                    device.play((playResponse) => {
+                        console.log(playResponse);
+                        return true;
+                    });
                 });
 
-                device.play((response) => {
-                    console.log(response);
-                    return true;
-                })
-            });
-
-            response.entries.forEach((element) => {
-                console.log(element.name);
-                const fileFullPath = {
-                    path: `${config.get('dropboxPath')}/${element.name}`
-                };
-                dbx.filesDelete(fileFullPath);
-            });
-        } else {
-            console.log('Nothing here');
-        }
-    })
-    .catch((error) => {
-        console.log(error.error);
-        schedule.cancel();
-    });
+                response.entries.forEach((element) => {
+                    console.log(element.name);
+                    const fileFullPath = {
+                        path: `${config.get('dropboxPath')}/${element.name}`,
+                    };
+                    dbx.filesDelete(fileFullPath);
+                });
+            } else {
+                console.log('Nothing here');
+            }
+        })
+        .catch((error) => {
+            console.log(error.error);
+            schedule.cancel();
+        });
 });
